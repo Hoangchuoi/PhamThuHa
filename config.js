abbf6b422549dfba80270496c7c7378a10a08230
Hoangchuoi <86770286+Hoangchuoi@users.noreply.github.com>
2021-07-01 21:01:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Bé yêu à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    introTitle: 'He luu bé yêu (〃ω〃)',
+    introDesc: `Cún có điều này muốn nói, bé nhớ phải trả lời thật lòng nhaaaa (〃▽〃)`,
+    btnIntro: 'vâng :3',
+    title: 'Bé có yêu cún không (⺣ﻌ⺣)',
+    desc: 'Nếu bé không trả lời là anh khóc cha xem 😖😖😖',
+    btnYes: 'Cóa yêu lắm lắm lun <3',
+    btnNo: 'Không yêu :>>>',
+    question:'Cho cún biết lý do được hong 😳😳😳',
+    btnReply: 'Gửi lời đến cún ♥️',
+    reply: 'vì cún rất dễ thương siêu siêu dễ thương :3',
+    mess: 'Thật hạ ☺️☺️☺️, anh xin lỗi vì nhiều lần đã làm e buồn ☹️, có nhiều thứ a không thể làm đúng như lời nói của mình 😫, nhưng e yên tâm nha a sẽ yêu e hơn ngày hôm qua và ngày mai và mãi mãi. 𝓐𝓝𝓗 𝓨Ê𝓤 𝓔𝓜 ♥️',
+    messDesc: 'Đến đây là hết mất ròi, chỉ muốn nói là a thương e lắm, mãi bên nhau nha☺️☺️☺️',
+    btnAccept: 'Dạaaaaaaaaa♥️♥️♥️',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
